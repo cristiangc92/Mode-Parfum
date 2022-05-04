e7712420a3ebd4417c542b82e7a4cf1771ca0dc5
Joacoruarte <ruartejoaquin@gmail.com>
2022-05-04 15:26:22 -0300

    a

--- Client/src/index.js
+++ Client/src/index.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import { Provider } from "react-redux";
 import store from "./redux/store/store";
 
-axios.defaults.baseURL = "http://localhost:3001"
+axios.defaults.baseURL = "https://back-mode-parfum.herokuapp.com"
 
 
 ReactDOM.render(
